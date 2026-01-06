3db633b2eff13b0ce5f1d2642fe97e0df481def5
Sushil Kumar <142862724+mrsushil67@users.noreply.github.com>
2026-01-06 21:44:25 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,31 +2,31 @@ const axios = require('axios');
 
 const getToken = async () => {
     try {
-        const url = 'https://api.fleetx.io/api/v1/login';
-        const data = {
-            username: 'Diljeet.singh+0038@fleetx.io',
-            password: 'EPqw59tO@',
-            grant_type: 'password',
-        };
+        // const url = 'https://api.fleetx.io/api/v1/login';
+        // const data = {
+        //     username: 'Diljeet.singh+0038@fleetx.io',
+        //     password: 'EPqw59tO@',
+        //     grant_type: 'password',
+        // };
 
-        const response = await axios.post(url, data, {
-            headers: {
-                Authorization: 'Basic ZmxlZXR4OnNlY3JldA==',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            httpsAgent: new (require('https').Agent)({
-                rejectUnauthorized: false,
-            }),
-            transformRequest: [
-                (data) => {
-                    return Object.keys(data)
-                        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-                        .join('&');
-                },
-            ],
-        });
+        // const response = await axios.post(url, data, {
+        //     headers: {
+        //         Authorization: 'Basic ZmxlZXR4OnNlY3JldA==',
+        //         'Content-Type': 'application/x-www-form-urlencoded',
+        //     },
+        //     httpsAgent: new (require('https').Agent)({
+        //         rejectUnauthorized: false,
+        //     }),
+        //     transformRequest: [
+        //         (data) => {
+        //             return Object.keys(data)
+        //                 .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        //                 .join('&');
+        //         },
+        //     ],
+        // });
 
-        return response.data.access_token;
+        return process.env.FLEETX_TOKEN;
     } catch (error) {
         if (error.response) {
             console.error('Error getting token (response):', error.response.data); // Log the response data
